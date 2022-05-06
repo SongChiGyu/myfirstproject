b84995ef46a3cffc8481176f72134698781b4d8b
sck0303 <sck0303@naver.com>
2022-05-06 17:22:17 +0900

    commit

--- CarSideSite/carkinds.js
+++ CarSideSite/carkinds.js
@@ -2,7 +2,7 @@ let mainText = document.querySelector("h1");
 
 window.addEventListener('scroll',function () {
     let value = window.scrollY;
-    // console.log("scrollY",value);
+    console.log("scrollY",value);
 
     if (value >= 400) {
         mainText.style.animation = 'disappear 2s ease-out forwards';
@@ -12,4 +12,15 @@ window.addEventListener('scroll',function () {
         mainText.style.animation = 'slide 2s ease-out';
     }
 });
+
+// let subText = document.querySelector("p");
+
+// window.addEventListener('scroll',function () {
+//     let value = window.scrollY;
+//     // console.log("scrollY",value);
+
+//     if (value >= 500) {
+//         mainText.style.animation = 'grow 2s ease-out';
+//     }
+// });
 
