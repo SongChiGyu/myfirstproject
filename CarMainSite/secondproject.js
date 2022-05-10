1cb50a756de529c640196c3cc80881a0053f0866
sck0303 <sck0303@naver.com>
2022-05-10 17:07:00 +0900

    commit

---
+++ CarMainSite/secondproject.js
@@ -0,0 +1,55 @@
+let subText = document.querySelector(".textany1");
+
+window.addEventListener('scroll',function () {
+    let value = window.scrollY;
+    console.log("scrollY",value);
+
+    if (value >= 1000) {
+        subText.style.animation = 'grow1 1s';        
+    } 
+    else {
+        subText.style.animation = 'grow2 1s forwards';      
+    }
+});
+
+let goodText1 = document.querySelector(".textany2");
+
+window.addEventListener('scroll',function () {
+    let value = window.scrollY;
+    
+
+    if (value >= 1900) {
+        goodText1.style.animation = 'grow1 1s';        
+    } 
+    else {
+        goodText1.style.animation = 'grow2 1s forwards';      
+    }
+});
+
+let goodText2 = document.querySelector(".textany3");
+
+window.addEventListener('scroll',function () {
+    let value = window.scrollY;
+    
+
+    if (value >= 2800) {
+        goodText2.style.animation = 'grow1 1s';        
+    } 
+    else {
+        goodText2.style.animation = 'grow2 1s forwards';      
+    }
+});
+
+let goodText3 = document.querySelector(".textany4");
+
+window.addEventListener('scroll',function () {
+    let value = window.scrollY;
+    
+
+    if (value >= 3700) {
+        goodText3.style.animation = 'grow1 1s';        
+    } 
+    else {
+        goodText3.style.animation = 'grow2 1s forwards';      
+    }
+});
--- CarSideSite/carkinds.js
+++ CarSideSite/carkinds.js
@@ -139,11 +139,30 @@ window.addEventListener('scroll',function () {
     }
 });
 
-$(document).ready(function(){
-    var currentPosition = parseInt($(".quickmenu").css("top")); 
-    $(window).scroll(function() { 
-        var position = $(window).scrollTop(); 
-        $(".quickmenu").stop().animate({"top":position+currentPosition+"px"},1000); 
-    }); 
-});
+jQuery(document).ready(function() {
+    var scrollSmoth = function(target) {
+        if (0 <= jQuery(window).scrollTop()) {
+            target.stop().animate({
+                 // #subNav를 수직중앙에 놓기위한 계산식
+                top: (jQuery(window).scrollTop() + (jQuery(window).height() * 0.5)) - (target.height() / 2)
+            }, 400);
+        }
+    }
+    scrollSmoth(jQuery('#subNav'));
 
+    jQuery(window).on('scroll', function() {
+        scrollSmoth(jQuery('#subNav'));
+    });
+
+    var i;
+    var objTop = new Array;
+    for (i = 0; i < jQuery('#contentWrap > div').length; i++) {
+        objTop.push(jQuery('#contentWrap > div').eq(i).offset().top);
+    }
+
+    jQuery('#subNav > ul > li').on('click', function() {
+        jQuery("html, body").stop().animate({
+            scrollTop: objTop[jQuery('li').index(this)],
+        }, 400);
+    })
+});
